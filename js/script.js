58fc3704602c56bd7586e8db4c36e09dbde6486c
Maxim <maximum454@gmail.com>
2015-08-28 17:33:24 +0600

    Верстка главной

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 $(function(){
+	$(".bx-main").bxSlider({
+
+	})
+
+	$(".bx-excursion").bxSlider({
+		slideWidth: 320,
+	    minSlides: 2,
+	    maxSlides: 3,
+	    slideMargin: 20,
+	    pager:0
+	})
+	
+	$(".bx-reviews").bxSlider({
+		pager:1,
+		controls:0
+	})
 	$('.popap_box').click(function() {
 		$('#modalbox_' + $(this).data('body')).arcticmodal();
 	});
